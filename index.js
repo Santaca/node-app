7362cf91b8036f7e0b78eadf598a22f9e366f9b0
Santaca <38440843+Santaca@users.noreply.github.com>
2022-03-16 14:01:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
+  console.log('Cambio');
   console.log(`Server running at http://${hostname}:${port}/`);
 });
